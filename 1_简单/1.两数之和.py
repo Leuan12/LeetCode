d938303984857533e5deb557e51648d3bd684527
Yao <mryao.com@gmail.com>
2019-07-26 20:52:33 +0800

    创建leetcode文件

---
+++ 1_简单/1.两数之和.py
@@ -0,0 +1,39 @@
+"""
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+"""
+
+"""
+enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下标，一般用在 for 循环当中。
+"""
+
+# 方法1:
+class Solution():
+	def twoSum(self,nums,target):
+		"""
+		:param nums: List[int]
+		:param target:
+		:return:
+		"""
+		hashmap = {}
+		for index, num in enumerate(nums):
+			another_num = target - num
+			if another_num in hashmap:
+				return [hashmap[another_num], index]
+			hashmap[num] = index
+		return None
+
+nums = [2,7,11,15]
+
+target = 10
+if __name__ == '__main__':
+	a = Solution()
+	a.twoSum(nums,target)
